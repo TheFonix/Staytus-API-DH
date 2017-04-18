21999f657c13e91a94474df4a483fa2c443a87eb
TheFonix <actualfriction@gmail.com>
2017-04-18 20:19:52 +0100

    Updating to use the Spinner

--- beta.sh
+++ beta.sh
@@ -1,23 +1,56 @@
-#Fonix Hardcastle API for Staytus
+#!/bin/bash
+#==============27/3/2017===============#
+#Created By Fonix: https://fonix.online#
+#Status Script for the Digital Hazards #
+#           status system              #
+#======================================#
 
-#Server Address: (IP or HTTP)
-SERVER='https://digitalhazards.net/'
-#Enable Debug Settings, This will output all the information to console
+source "https://apt.fonix.online/spinner.sh" #Conection to Loader
 
-OFF="Offline but Pinged"
-ON="Online and Working"
-TONULL="/dev/null 2>&1"
+			 #=========#
+			 #Variables:
+			 #=========#
+SERVER='https://digitalhazards.net/' #Server Address: (IP or HTTP)
+OFF="Could no be contacted, but it was pinged" # Message if the server didnt respond
+ON="Server was pinged and got a response" #Message if the server responded
+TONULL="/dev/null 2>&1" # NUll var, to send info to a place other then the console
+API="Staytus API" #Name of the Status API
 
-/usr/bin/curl -sSf $SERVER > "$TONULL"
-CS=$?
 
-    if [ $CS -ne 0 ]
-    then
-    	echo "$SERVER is $OFF"
-        curl --header 'X-Auth-Token: TOKEN' --header 'X-Auth-Secret: TOKEN' -H "Content-Type: application/json" -d '{"service":"dhio1","status":"major-outage"}' -v http://status.digitalhazards.net/api/v1/services/set_status > /dev/null 2>&1
+	echo ""
 
-    elif [ $CS -eq 0 ]
-    then
-        echo "$SERVER is $ON"
-        curl --header 'X-Auth-Token: TOKEN' --header 'X-Auth-Secret: TOKEN' -H "Content-Type: application/json" -d '{"service":"dhio1","status":"operational"}' -v http://status.digitalhazards.net/api/v1/services/set_status > /dev/null 2>&1
-    fi
+	echo "Current Server to check is $SERVER"
+
+	echo ""
+
+	start_spinner 'Checking Site...'
+
+		/usr/bin/curl -sSf $SERVER > "$TONULL"
+			CS=$?
+
+	echo ""
+	stop_spinner $?
+
+	echo "Connection Complete"
+
+
+    	if [ $CS -ne 0 ]
+
+    		then
+    			start_spinner '$SERVER $OFF'
+    			sleep 2
+        		curl --header 'X-Auth-Token: TOKEN' --header 'X-Auth-Secret: TOKEN' -H "Content-Type: application/json" -d '{"service":"dhio1","status":"major-outage"}' -v http://status.digitalhazards.net/api/v1/services/set_status > /dev/null 2>&1
+         		 stop_spinner $?
+    		elif [ $CS -eq 0 ]
+    		then
+    			start_spinner '$SERVER $ON'
+        		sleep 2
+        		curl --header 'X-Auth-Token: TOKEN' --header 'X-Auth-Secret: TOKEN' -H "Content-Type: application/json" -d '{"service":"dhio1","status":"operational"}' -v http://status.digitalhazards.net/api/v1/services/set_status > /dev/null 2>&1
+        		 stop_spinner $?
+    	fi
+
+    echo ""
+
+	echo "Script Complete, Information from $SERVER was sent to the $API"
+
+	echo ""
